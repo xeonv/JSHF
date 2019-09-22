7654c506a1352909b0121f6e517be51d0061640e
Valery <xeonv@mail.ru>
2019-09-22 13:51:23 +0400

    001

--- 001/script/test.js
+++ 001/script/test.js
@@ -1,6 +1,13 @@
-"use strict";
+//"use strict";
 
 setTimeout(message,1000);
 function message() {
 	alert("ПРИВЕТ")
 };
+
+let element = document.getElementById('test');
+let scoops = 5;
+while (scoops > 0) {
+	element = 'Another scoop! <br>';
+	scoops +=1;
+}
