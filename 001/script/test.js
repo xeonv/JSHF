b64554b1a3ba20a7200575b917fb800bb4fc5d76
Valery <xeonv@mail.ru>
2019-09-22 14:37:00 +0400

    happy birthday

--- 001/script/test.js
+++ 001/script/test.js
@@ -1,13 +1,33 @@
-//"use strict";
+"use strict";
 
-setTimeout(message,1000);
-function message() {
-	alert("ПРИВЕТ")
-};
+// setTimeout(message,1000);
+// function message() {
+// 	alert("ПРИВЕТ")
+// };
 
-let element = document.getElementById('test');
-let scoops = 5;
-while (scoops > 0) {
-	element = 'Another scoop! <br>';
-	scoops +=1;
+
+// let scoops = 5;
+// while (scoops > 0) {
+// 	document.write('Another scoop! <br>');
+// 	scoops -=1;
+// }
+
+// let elemKey = [];
+// let element = document.getElementById('test');
+// for (key in element) {
+// 	elemKey.push(key);
+// }
+
+// document.write(elemKey);
+
+// alert(elemKey.innerText);
+
+let name = "Joe";
+let i = 0;
+while (i < 2) {
+	document.write("Happy Birthday to you. <br>");
+	i += 1;
 }
+
+document.write("Happy Birthday dear " + name + ",<br>");
+document.write("Happy Birthday to you. <br>");
