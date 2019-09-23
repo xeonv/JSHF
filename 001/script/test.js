f4503f6cf2e9cd31f3c26aa2330b28feb937ca1f
Valery <xeonv@mail.ru>
2019-09-23 20:50:38 +0400

    array

--- 001/script/test.js
+++ 001/script/test.js
@@ -22,12 +22,71 @@
 
 // alert(elemKey.innerText);
 
-let name = "Joe";
-let i = 0;
-while (i < 2) {
-	document.write("Happy Birthday to you. <br>");
-	i += 1;
+// let name = "Joe";
+// let i = 0;
+// while (i < 2) {
+// 	document.write("Happy Birthday to you. <br>");
+// 	i += 1;
+// }
+
+// document.write("Happy Birthday dear " + name + ",<br>");
+// console.log("Happy Birthday to you. <br>");
+
+// let arr =[2,3,6,7];
+// for (let index = 0; arr.length - 1; index++ ) {
+// 	alert( arr[index]);
+// } 
+
+// let arr = [1,2,3,4,5,];
+// alert(arr.reduce((sum,current) => sum + current, 0));
+
+// for (let index in arr) {
+// 	alert (index);
+// }
+
+
+// function camelize(string) {
+// 	let arr = string.split('');
+// 	arr.forEach(function (item, index) {
+// 			if (item == '-') {
+// 				arr.splice(index,1)
+// 				arr[index] = arr[index].toUpperCase();
+// 			}
+// 	}
+// 	)
+// 	return arr.join('');
+
+// }
+
+
+// alert(camelize("background-color") == 'backgroundColor');
+// alert(camelize("list-style-image") == 'listStyleImage');
+// alert(camelize("-webkit-transition") == 'WebkitTransition');
+
+
+
+// function  filterRange(arr, a, b) {
+// 	return arr.filter(item => item >= a && item <= b)
+// }
+
+// let arr = [5, 3, 8, 1];
+
+// let filtered = filterRange(arr, 1, 4);
+
+// alert( filtered ); // 3,1 (совпадающие значения)
+
+// alert( arr ); // 5,3,8,1 (без изменений)
+
+function  filterRange(arr, a, b) {
+	arr.forEach( function (item,index) {
+		if (item < a || item > b) arr.splice(index,1);
+	} );
+	
 }
 
-document.write("Happy Birthday dear " + name + ",<br>");
-document.write("Happy Birthday to you. <br>");
+let arr = [5, 3, 8, 1];
+
+ filterRange(arr, 1, 4);
+
+
+alert( arr ); // 5,3,8,1 (без изменений)
