9d5f619f3c5b476b994ce42f538615afe0e071fc
Valery <xeonv@mail.ru>
2019-11-21 14:23:46 +0400

    Форматирование относительн

--- 001/script/test.js
+++ 001/script/test.js
@@ -145,43 +145,280 @@
 //-------------------------------------------------------------------------------------------
 
 
-function Calculator() {
+// function Calculator() {
 
-  this.methods = {
-    "-": (a, b) => a - b,
-    "+": (a, b) => a + b
-  };
+//   this.methods = {
+//     "-": (a, b) => a - b,
+//     "+": (a, b) => a + b
+//   };
 
-  this.calculate = function(str) {
+//   this.calculate = function(str) {
 
-    let split = str.split(' '),
-      a = +split[0],
-      op = split[1],
-      b = +split[2]
+//     let split = str.split(' '),
+//       a = +split[0],
+//       op = split[1],
+//       b = +split[2]
 
-    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-      return NaN;
-    }
+//     if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+//       return NaN;
+//     }
 
-    return this.methods[op](a, b);
+//     return this.methods[op](a, b);
+//   }
+
+//   this.addMethod = function(name, func) {
+//     this.methods[name] = func;
+//   };
+// }
+
+// let calc = new Calculator;
+
+// alert( calc.calculate("3 + 7") ); // 10
+
+// let powerCalc = new Calculator;
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+
+// let result = powerCalc.calculate("2 ** 3");
+// alert( result ); // 8
+
+
+//-------------------------------------------------------------------------------------------
+
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// let names = users.map(item => item.name);
+
+// alert( names ); // Вася, Петя, Маша
+
+
+//-------------------------------------------------------------------------------------------
+
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = users.map(function(item)  {
+// 	                                 let result = {fullName: `${item.name} ${item.surname}`, id: item.id};
+// 	                                 return result;
+// 	                                 }
+// 	                                 );
+
+// /*
+// usersMapped = [
+//   { fullName: "Вася Пупкин", id: 1 },
+//   { fullName: "Петя Иванов", id: 2 },
+//   { fullName: "Маша Петрова", id: 3 }
+// ]
+// */
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // Вася Пупкин
+
+//-------------------------------------------------------------------------------------------
+
+
+// function sortByAge (users) {
+// 	users.sort((a, b) => a.age > b.age ? 1 : -1);
+// };
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let arr = [ vasya, petya, masha ];
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// alert(arr[0].name); // Вася
+// alert(arr[1].name); // Маша
+// alert(arr[2].name); // Петя
+
+//-------------------------------------------------------------------------------------------
+
+// function shuffle(arr) {
+// 	arr.sort((a,b) => (Math.random() - 0.5));
+// }
+
+// let arr = [1, 2, 3];
+
+// shuffle(arr);
+// // arr = [3, 2, 1]
+// alert(arr);
+
+// shuffle(arr);
+// // arr = [2, 1, 3]
+// alert(arr);
+
+// shuffle(arr);
+// // arr = [3, 1, 2]
+// alert(arr);
+// // ...
+
+
+//-------------------------------------------------------------------------------------------
+
+
+// function shuffle(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
+
+// // подсчёт вероятности для всех возможных вариантов
+// let count = {
+//   '123': 0,
+//   '132': 0,
+//   '213': 0,
+//   '231': 0,
+//   '321': 0,
+//   '312': 0
+// };
+
+// for (let i = 0; i < 1000000; i++) {
+//   let array = [1, 2, 3];
+//   shuffle(array);
+//   count[array.join('')]++;
+// }
+
+// // показать количество всех возможных вариантов
+// for (let key in count) {
+//   alert(`${key}: ${count[key]}`);
+// }
+
+//-------------------------------------------------------------------------------------------
+
+// function getAverageAge(array) {
+// 	return array.reduce( (sum, current) => sum + current.age, 0 ) / array.length;
+// }
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 29 };
+
+// let arr = [ vasya, petya, masha ];
+
+// alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+
+//-------------------------------------------------------------------------------------------
+
+
+// function unique(arr) {
+  
+//   let result = [];
+
+//   for (let item of arr) {
+//   	if (!result.includes(item)) {
+//   		result.push(item);
+//   	}
+//   }
+  
+//   return result;
+  
+// }
+
+// let strings = ["кришна", "кришна", "харе", "харе",
+//   "харе", "харе", "кришна", "кришна", ":-O"
+// ];
+
+// alert( unique(strings) ); // кришна, харе, :-O
+
+
+//-------------------------------------------------------------------------------------------
+
+
+// let messages = [
+// {text: "Hello", from: "John"},
+// {text: "How goes?", from: "John"},
+// {text: "See you soon", from: "Alice"}
+// ];
+// let object1 = messages[0];
+// let x = new WeakMap();
+// x.set(object1, messages[0]);
+// alert("Чтение первого сообщения: " + x.has(object1));
+// message.shift();
+// alert("Чтение первого сообщения: " + x.has(object1));
+
+
+//-------------------------------------------------------------------------------------------
+
+function formatDate(date) {
+  let dayOfMonth = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let diffMs = new Date() - date;
+  let diffSec = Math.round(diffMs / 1000);
+  let diffMin = diffSec / 60;
+  let diffHour = diffMin / 60;
+
+  // форматирование
+  year = year.toString().slice(-2);
+  month = month < 10 ? '0' + month : month;
+  dayOfMonth = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
+
+  if (diffSec < 1) {
+    return 'прямо сейчас';
+  } else if (diffMin < 1) {
+    return `${diffSec} сек. назад`
+  } else if (diffHour < 1) {
+    return `${diffMin} мин. назад`
+  } else {
+    return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`
   }
-
-  this.addMethod = function(name, func) {
-    this.methods[name] = func;
-  };
 }
 
-let calc = new Calculator;
+alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
 
-alert( calc.calculate("3 + 7") ); // 10
+alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
 
-let powerCalc = new Calculator;
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
 
-let result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
+// вчерашняя дата вроде 31.12.2016, 20:00
+alert( formatDate(new Date(new Date - 86400 * 1000)) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
